b3c8e65949cd1a0cbc91cf881ebb6f2ee286e213
Logan Bell <lbell@shutterstock.com>
2013-04-22 17:58:03 -0400

    Rockified/misc
    * Added a .rock.yml file and cpanfile for deps.
    * Did some code clean-up around around how HTTP::Request objects are
      passed into the instance.
    * Added some Perl documentation.

--- lib/PrankCall.pm
+++ lib/PrankCall.pm
@@ -6,15 +6,12 @@ use HTTP::Request;
 use IO::Socket;
 use URI;
 
-# ABSTRACT: PrankCall Fast non blocking GET requests
-
 sub new {
   my ($class, %params) = @_;
 
   my $self = {
     host => $params{host},
     port => $params{port} ||= 80,
-    request_obj => $params{request_obj},
     # TODO: other params
   };
 
@@ -24,7 +21,7 @@ sub new {
 sub get {
   my ($self, %params) = @_;  
 
-  my $req = $self->{request_obj} || do {
+  my $req = $params{request_obj} || do {
     my $path = $params{path};
     my $params = $params{params};
 
@@ -34,16 +31,16 @@ sub get {
     $uri->query_form($params);
 
     my $req = HTTP::Request->new(GET => $uri->as_string);
-    $req->protocol("HTTP/1.1");
     $req;
   };
 
+  $req->protocol("HTTP/1.1");
   my $http_string = $req->as_string;
   my $port = $req->uri->port;
   my $raw_host = $req->uri->host;
 
-  $req->protocol("HTTP/1.1");
   my $error = do {
+    local $@;
     eval {
       my $remote = IO::Socket::INET->new( Proto => 'tcp', PeerAddr => $raw_host, PeerPort => $port ) || die "Ah shoot Johny $!";
       $remote->autoflush(1);
@@ -57,6 +54,36 @@ sub get {
     # TODO if they want errors?
     warn $error;
   }
+
+  return 1;
 }
 
 1;
+
+__END__
+
+=head1 NAME
+
+PrankCall - call remote services and hang up without waiting for a response
+
+=head1 SYNOPSIS
+
+    my $prank = PrankCall->new(
+        host => 'somewhere.beyond.the.sea',
+        port => '10827',
+    );
+
+    $prank->get(path => '/', params => { 'bobby' => 'darin' }); # note, prank calls always succeed
+
+=head1 DESCRIPTION
+
+Sometimes you just wanna call someone and hang up without waiting for them to say anything.
+PrankCall is your friend (but, oddly, also your nemesis).
+
+=head1 AUTHOR
+
+Logan Bell
+
+=head1 LICENSE
+
+This is released under the "Don't blame me" license. Don't blame me for this idea.
--- t/prank.t
+++ t/prank.t
@@ -25,10 +25,6 @@ ok $httpd;
 $obj = $class->new(host => 'http://127.0.0.1', port => $httpd->port);
 $obj->get(path => '/', params => { 'foo' => 'bar' });
 
-# This is sort of silly, but we have a async web server we need to have
-# in sync.
-sleep 2;
-
 $httpd = run_http_server {
   my $request = shift;
   my $path = $request->uri->path;
@@ -39,7 +35,7 @@ $httpd = run_http_server {
   return [ 200 , [ 'Content-Type' => 'text/plain'], ['Success!'] ];
 };
 
-$obj = $class->new(request_obj => HTTP::Request->new(GET => join('/', $httpd->endpoint, 'http_request')));
-$obj->get;
+$obj = $class->new;
+$obj->get( request_obj => HTTP::Request->new(GET => join('/', $httpd->endpoint, 'http_request')));
 
 sleep 2;
